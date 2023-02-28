79ead8b586b9382c1fc952ab2ae9d1ba1ab6b96c
zainabtaha1 <121588138+zainabtaha1@users.noreply.github.com>
2023-02-28 03:59:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+double x  = double.Parse(Console.ReadLine());
+double  y = double.Parse(Console.ReadLine());
+double result;
+Console.WriteLine("firstnumber: " + x);
+Console.WriteLine("secondnumber: " + y);
+result = x + y;
+Console.WriteLine(x +"+"+ y+"="+ result);
+Console.WriteLine("result : " + result);
+result = x - y;
+Console.WriteLine(x +"-"+ y +"="+ result);
+Console.WriteLine("result : " + result);
+result = x * y;
+Console.WriteLine(x +"*"+ y +"="+ result);
+Console.WriteLine("result : " + result);
+result = x / y;
+Console.WriteLine(x +"/"+ y +"="+ result);
+Console.WriteLine("result : " + result);
